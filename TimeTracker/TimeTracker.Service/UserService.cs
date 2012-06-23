3ec23953e292cc1bd04a0eeab5f9ebc32809b9fc
nineoh <n_lanfranchi@hotmail.com>
2012-06-23 18:07:07 +0200

    Initial UserService and UserServiceTest classes created

---
+++ TimeTracker/TimeTracker.Service/UserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeTracker.Data;
+
+namespace TimeTracker.Service
+{
+	public class UserService : IService<User>
+	{
+		private readonly TimeTrackerDataModelContainer _modelContainer;
+
+		public UserService(TimeTrackerDataModelContainer modelContainer)
+		{
+			_modelContainer = modelContainer;
+		}
+
+		public User GetById(int id)
+		{
+			return _modelContainer.User.FirstOrDefault(u => u.Id == id);
+		}
+
+		public IList<User> Find(Func<User, bool> search)
+		{
+			return _modelContainer.User.Where(search).ToList();
+		}
+
+		public void Save(User entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(User entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
