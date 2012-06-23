b23473db18ca7ac7f6ad935e819191f859e67974
YvesM <yves.mundorff@gmail.com>
2012-06-23 16:02:34 +0200

    Add Castle Windsor IoC container.

---
+++ TimeTracker/TimeTracker.Service/IService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeTracker.Service
+{
+    public interface IService<TEntity>
+    {
+        TEntity GetById();
+        IList<TEntity> Find(Func<TEntity, bool> search);
+        void Save(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
